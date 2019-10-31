a3b5e20bd8e9531a5e6903152057c5908ce99feb
Blasy <Blaaaasy@users.noreply.github.com>
2019-10-31 16:42:41 +0800

    news

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -56,72 +56,21 @@
           </div>
         </div>
       </div>
-      <!-- <div class="row">
-        <div class="card shadow-sm col-md-3">
-          <div class="card-img-top mt-4">
-            <img src="@/assets/img/Home/tech.png" alt="技术" width="100" height="100" />
-          </div>
-          <div class="card-body">
-            <h2 class="card-title">技术部</h2>
-          </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">项目成果</li>
-            <li class="list-group-item">编程教学</li>
-            <li class="list-group-item">人工智能</li>
-          </ul>
-          <div class="card-body">
-            <a href="/tech" class="card-link">更多</a>
+      <template v-for="(item, index) in this.news">
+        <div :key="index">
+          <div class="row">
+            <div class="col-md-8">
+              {{item.title}}
+            </div>
+            <div class="col-md-2">
+              {{item.date}}
+            </div>
+            <div class="col-md-2">
+              {{item.author}}
+            </div>
           </div>
         </div>
-        <div class="card shadow-sm col-md-3">
-          <div class="card-img-top mt-4">
-            <img src="@/assets/img/Home/serve.png" alt="服务" width="100" height="100" />
-          </div>
-          <div class="card-body">
-            <h2 class="card-title">服务部</h2>
-          </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">免费维修</li>
-            <li class="list-group-item">软件安装</li>
-            <li class="list-group-item">日常维护</li>
-          </ul>
-          <div class="card-body">
-            <a href="/service" class="card-link">更多</a>
-          </div>
-        </div>
-        <div class="card shadow-sm col-md-3">
-          <div class="card-img-top mt-4">
-            <img src="@/assets/img/Home/media.png" alt="组宣" width="100" height="100" />
-          </div>
-          <div class="card-body">
-            <h2 class="card-title">组宣部</h2>
-          </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">微信公众号</li>
-            <li class="list-group-item">QQ运营</li>
-            <li class="list-group-item">活动宣传</li>
-          </ul>
-          <div class="card-body">
-            <a href="javascript:void(0);" class="card-link">更多</a>
-          </div>
-        </div>
-        <div class="card shadow-sm col-md-3">
-          <div class="card-img-top mt-4">
-            <img src="@/assets/img/Home/game.png" alt="电竞" width="100" height="100" />
-          </div>
-          <div class="card-body">
-            <h2 class="card-title">电竞部</h2>
-          </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">观看比赛</li>
-            <li class="list-group-item">校内比赛</li>
-            <li class="list-group-item">校外比赛</li>
-          </ul>
-          <div class="card-body">
-            <a href="/e-sports" class="card-link">更多</a>
-          </div>
-        </div>
-      </div>-->
+      </template>
     </div>
   </div>
 </template>
@@ -162,6 +111,23 @@ export default {
           img: require("@/assets/img/Home/bg-th3.png"),
           title: "",
           subTitle: "ECUST E-Sports, Go Beyond the Game"
+        }
+      ],
+      news: [
+        {
+          title: "1",
+          date: "2019/10/31",
+          author: "jishu"
+        },
+        {
+          title: "2",
+          date: "2019/10/31",
+          author: "jishu"
+        },
+        {
+          title: "3",
+          date: "2019/10/31",
+          author: "jishu"
         }
       ]
     };
